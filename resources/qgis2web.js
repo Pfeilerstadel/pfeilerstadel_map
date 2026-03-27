f935f6e85619826c0bad212c0d448b9a0bf08a79
Pfeilerstadel <christopher.juwan@icloud.com>
2026-03-27 10:52:08 +0100

    new upload

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -504,11 +504,11 @@ var centerContainerDiv = document.getElementById('center-container')
 var Title = new ol.control.Control({
     element: (() => {
         var titleElement = document.createElement('div');
-        titleElement.className = 'bottom-right-title ol-control';
+        titleElement.className = 'top-left-title ol-control';
         titleElement.innerHTML = '<h2 class="project-title">Pfeilerstadel in Kärnten</h2>';
         return titleElement;
     })(),
-    target: 'bottom-right-container'
+    target: 'top-left-container'
 });
 map.addControl(Title)
     
